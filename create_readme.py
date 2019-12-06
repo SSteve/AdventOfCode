63c2e8031b0f083cd7bdda14e8b8bcd785b3ec7e
Steve Nicholson <ssteve@mac.com>
2019-12-06 11:31:56 -0800

    Deal with two solutions for one day

--- create_readme.py
+++ create_readme.py
@@ -27,8 +27,13 @@ def create_readme(year):
 	for day in range(1, 26):
 		if os.path.isfile(f"Advent{year}/{day}.py"):
 			print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + [solution](./{day}.py)")
+		elif os.path.isfile(f"Advent{year}/{day}a.py"):
+			if os.path.isfile(f"Advent{year}/{day}b.py"):
+				print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + solutions: [day 1](./{day}a.py), [day 2](./{day}b.py)")
+			else:
+				print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + solutions: [day 1](./{day}a.py)")
 		else:
 			print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day})")
 
 if __name__ == "__main__":
-	create_readme(2015)
+	create_readme(2019)
