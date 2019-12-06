7934e4e70579fc329557e6b473fe3d6086a3d300
Steve Nicholson <ssteve@mac.com>
2019-12-06 11:15:27 -0800

    Write program to generate README file for a puzzle year

---
+++ create_readme.py
@@ -0,0 +1,34 @@
+import os
+
+THIS_YEAR = 2019
+
+def create_readme(year):
+	print(f"# Advent of Code {year}")
+	print()
+	print(f"## Solutions to [Advent of Code {year}](https://adventofcode.com/{year}/) puzzles")
+	print()
+	print("### Jump to")
+	for link_year in range(2015, THIS_YEAR + 1):
+		if link_year == year:
+			continue;
+		print(f"- [{link_year}](https://github.com/SSteve/AdventOfCode/tree/master/Advent{link_year})")
+	print()
+	
+	# Include notes for this year
+	try:
+		for notes_line in open(f"Advent{year}/notes.txt"):
+			print(notes_line)
+	except:
+		# Don't do anything if notes.txt doesn't exist
+		pass
+		
+	print("### The Puzzles")
+	
+	for day in range(1, 26):
+		if os.path.isfile(f"Advent{year}/{day}.py"):
+			print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + [solution](./{day}.py)")
+		else:
+			print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day})")
+
+if __name__ == "__main__":
+	create_readme(2015)
