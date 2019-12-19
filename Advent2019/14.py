8916c87ad147f767620bae39a7fdfadbea7791d2
Steve Nicholson <ssteve@mac.com>
2019-12-18 23:40:49 -0800

    Started day 14

---
+++ Advent2019/14.py
@@ -0,0 +1,110 @@
+import re
+
+from typing import Iterable, List, NamedTuple
+
+REACTIONS1 = """10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL"""
+
+REACTIONS2 = """9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL"""
+
+REACTIONS3 = """157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"""
+
+REACTIONS4 = """2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF"""
+
+REACTIONS5 = """171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX"""
+
+compiled_element = re.compile(r"\s*(\d*)\s*(\D*)")
+
+class CompoundQuantity(NamedTuple):
+    quantity: int
+    compound: str
+
+class Reaction:
+    def __init__(self, quantities: Iterable[str], output: str):
+        self.reaction_inputs: List[CompoundQuantity] = []
+        for quantity in quantities:
+            match = re.match(compiled_element, quantity)
+            amount = int(match[1])
+            chemical = match[2]
+            self.reaction_inputs.append(CompoundQuantity(amount, chemical))
+        match = re.match(compiled_element, output)
+        self.reaction_output = CompoundQuantity(int(match[1]), match[2])
+        print(self.reaction_output)
+
+
+class NanoFactory:
+    def __init__(self, reaction_strings: Iterable[str]):
+        self.reactions: List[Reaction] = []
+        for reaction in reaction_strings:
+            input_output = reaction.strip().split(" => ")
+            inputs = input_output[0].split(",")
+            self.reactions.append(Reaction(inputs, input_output[1]))
+
+
+def ore_cost(factory: NanoFactory) -> int:
+    return 0
+
+factory = NanoFactory(REACTIONS1.split("\n"))
+#assert(ore_cost(factory) == 31)
+
+factory = NanoFactory(REACTIONS2.split("\n"))
+#assert(ore_cost(factory) == 165)
+
+factory = NanoFactory(REACTIONS3.split("\n"))
+#assert(ore_cost(factory) == 13312)
+
+factory = NanoFactory(REACTIONS4.split("\n"))
+#assert(ore_cost(factory) == 180697)
+
+factory = NanoFactory(REACTIONS5.split("\n"))
+#assert(ore_cost(factory) == 2210736)
+
+with open("14.txt") as infile:
+    factory = NanoFactory(infile)
+
+print(factory.reactions[0])
