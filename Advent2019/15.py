6d6d60f2188cc5134c73da7d266cab452cd2dab4
Steve Nicholson <ssteve@mac.com>
2019-12-23 05:02:16 -0800

    Fix syntax errors

--- Advent2019/15.py
+++ Advent2019/15.py
@@ -1,30 +1,30 @@
-from __future__ import Annotations
-from enum import Enum
+from __future__ import annotations
+from enum import IntEnum
 from generic_search import bfs
 from intcode import IntCode
 from typing import Dict, NamedTuple
 
-class Direction(Enum):
-	NORTH = 1
-	SOUTH = 2
-	WEST = 3
-	EAST = 4
+class Direction(IntEnum):
+    NORTH = 1
+    SOUTH = 2
+    WEST = 3
+    EAST = 4
 
 class Point(NamedTuple):
     x: int
     y: int
 
     def north(self) -> Point:
-        return Point(x, y - 1)
+        return Point(self.x, self.y - 1)
 
     def south(self) -> Point:
-        return Point(x, y + 1)
+        return Point(self.x, self.y + 1)
 
     def west(self) -> Point:
-        return Point(x - 1, y)
+        return Point(self.x - 1, self.y)
 
     def east(self) -> Point:
-        return Point(x + 1, y)
+        return Point(self.x + 1, self.y)
         
     def around(self) -> Dict[Direction, Point]:
         return {Direction.NORTH: self.north(), Direction.SOUTH: self.south(), Direction.WEST: self.west(), Direction.EAST: self.east()}
@@ -40,15 +40,17 @@ class Droid:
     def successors(self, point: Point) -> List[Point]:
         result: List[Point] = []
         for direction, new_point in point.around().items():
-        	location_code = self.map.get(new_point, -1)
-        	if location_code == -1:
-        		self.computer.accept_input(direction)
-        		self.computer.run()
-        		location_code == self.computer.output_values.pop(0)
-        		self.map[new_point] = location_code
-        if location_code != 0:
-				result.append(new_point)
-		return result
+            location_code = self.map.get(new_point, -1)
+            if location_code == -1:
+                print(f"Direction: {direction}")
+                self.computer.accept_input(direction)
+                self.computer.run()
+                location_code == self.computer.output_values.pop(0)
+                self.map[new_point] = location_code
+                print(new_point)
+            if location_code != 0:
+                result.append(new_point)
+        return result
 
 if __name__ == '__main__':
     with open("15.txt") as infile:
@@ -56,4 +58,5 @@ if __name__ == '__main__':
 
     droid = Droid(computer)
 
-    solution = bfs(droid.location, droid.found_oxygen_system, droid.successors)
+    solution = bfs(Point(0, 0), droid.found_oxygen_system, droid.successors)
+    print(solution)
