3f59817f0365aab8b53cd392b75aae3153dbe0b1
Steve Nicholson <ssteve@mac.com>
2020-11-20 11:22:26 -0800

    Include Swift solutions in README.

--- create_readme.py
+++ create_readme.py
@@ -1,39 +1,42 @@
 import os
+import sys
+from sys import argv
 
-THIS_YEAR = 2019
+THIS_YEAR = 2020
 
-def create_readme(year):
-	print(f"# Advent of Code {year}")
-	print()
-	print(f"## Solutions to [Advent of Code {year}](https://adventofcode.com/{year}/) puzzles")
-	print()
-	print("### Jump to")
-	for link_year in range(2015, THIS_YEAR + 1):
-		if link_year == year:
-			continue;
-		print(f"- [{link_year}](https://github.com/SSteve/AdventOfCode/tree/master/Advent{link_year})")
-	print()
-	
-	# Include notes for this year
-	try:
-		for notes_line in open(f"Advent{year}/notes.txt"):
-			print(notes_line)
-	except:
-		# Don't do anything if notes.txt doesn't exist
-		pass
-		
-	print("### The Puzzles")
-	
-	for day in range(1, 26):
-		if os.path.isfile(f"Advent{year}/{day}.py"):
-			print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + [solution](./{day}.py)")
-		elif os.path.isfile(f"Advent{year}/{day}a.py"):
-			if os.path.isfile(f"Advent{year}/{day}b.py"):
-				print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + solutions: [day 1](./{day}a.py), [day 2](./{day}b.py)")
-			else:
-				print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day}) + solutions: [day 1](./{day}a.py)")
-		else:
-			print(f"- Dec {day}. - [instructions](http://adventofcode.com/{year}/day/{day})")
+
+def create_readme(year: int):
+    print(f"# Advent of Code {year}")
+    print()
+    print(f"## Solutions to [Advent of Code {year}](https://adventofcode.com/{year}/) puzzles")
+    print()
+    print("### Jump to")
+    for link_year in range(2015, THIS_YEAR + 1):
+        if link_year == year:
+            continue
+        print(f"- [{link_year}](https://github.com/SSteve/AdventOfCode/tree/master/Advent{link_year})")
+    print()
+
+    # Include notes for this year
+    if os.path.isfile(f"Advent{year}/notes.txt"):
+        for notes_line in open(f"Advent{year}/notes.txt"):
+            print(notes_line)
+
+    print("### The Puzzles")
+
+    for day in range(1, 26):
+        day_string = f"- Dec {day} - [instructions](http://adventofcode.com/{year}/day/{day})"
+        if os.path.isfile(f"Advent{year}/{day}.py"):
+            day_string += f" - [solution](./{day}.py) (Python)"
+        if os.path.isfile(f"Advent{year}/{day}a.py"):
+            day_string += f" - [part 1 solution](./{day}a.py) (Python)"
+        if os.path.isfile(f"Advent{year}/{day}b.py"):
+            day_string += f" - [part 2 solution](./{day}b.py) (Python)"
+        if os.path.isfile(f"Advent{year}/{day}.swift"):
+            day_string += f" - [solution](./{day}.swift) (Swift)"
+        print(day_string)
+
 
 if __name__ == "__main__":
-	create_readme(2019)
+    year = THIS_YEAR if len(argv) < 2 else int(argv[1])
+    create_readme(year)
