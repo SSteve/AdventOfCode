74d275313bc73e913cf609526de9afdaf6ebaff5
Steve Nicholson <SSteve@users.noreply.github.com>
2020-12-06 06:19:22 -0800

    2020 Day 6 Python code cleanup
    Still in bed on my iPad screen.

--- Advent2020/6.py
+++ Advent2020/6.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 TEST = """abc
 
@@ -18,46 +18,48 @@ a
 b"""
 
 def ReadForms(formLines: List[str]) -> List:
-    groups: List[(int, Dict)] = []
+    groups: List[Tuple[int, Dict[str, int]]] = []
     
-    form = defaultdict(int)
+    group = defaultdict(int)
     formCount = 0
     for formLine in formLines:
         if len(formLine) == 0:
-            groups.append((formCount, form))
-            form = defaultdict(int)
+            groups.append((formCount, group))
+            group = defaultdict(int)
             formCount = 0
         else:
             formCount += 1
             for answer in formLine:
-                form[answer] += 1
+                group[answer] += 1
     if formCount > 0:
-        groups.append((formCount, form))
+        # We got to the end of the file while still evaluating a group.
+        groups.append((formCount, group))
     return groups
-        
+    
+def CountAny(groups: List[Tuple[int, Dict[str, int]]]) -> int:
+    # Return the sum of the total number of letters in each group.
+    return sum(len(group[1].keys()) for group in groups)
+    
+def CountAll(groups: List[Tuple[int, Dict[str, int]]]) -> int:
+    # Return the sum of the number of letters that appeared on
+    # every form in the group.
+    total = 0
+    for count, group in groups:
+        total += sum(group[letter] == count for letter in group)
+    return total
     
 testGroups = ReadForms(TEST.splitlines())
-testAnswer = sum(len(group[1].keys()) for group in testGroups)
+testAnswer = CountAny(testGroups)
 assert testAnswer == 11, f"Test answer is {testAnswer}. It should be 11."
-testAnswer2 = 0
-for count, group in testGroups:
-    for letter in group:
-        if group[letter] == count:
-            testAnswer2 += 1
+testAnswer2 = CountAll(testGroups)
 assert testAnswer2 == 6, f"Test answer 2 is {testAnswer2}. It should be 6."
         
 
 with open("6.txt", "r") as infile:
     groups = ReadForms(infile.read().splitlines())
-for count, group in groups:
-    print(f"count: {count} keys: {group.keys()}")
-answer = sum(len(group[1].keys()) for group in groups)
-print(answer)
+answer = CountAny(groups)
+print(f"Part 1: {answer}")
 
-answer2 = 0
-for count, group in groups:
-    for letter in group:
-        if group[letter] == count:
-            answer2 += 1
+answer2 = CountAll(groups)
 print(f"Part 2: {answer2}")
 
