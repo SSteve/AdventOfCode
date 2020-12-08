3eecbf5a065d308afa54342e60ed525209f30be9
Steve Nicholson <ssteve@mac.com>
2020-12-08 11:05:24 -0800

    Advent 2020 Day 8 Python code cleanup

--- Advent2020/8.py
+++ Advent2020/8.py
@@ -1,8 +1,11 @@
-from collections import namedtuple
+from dataclasses import dataclass
 from typing import List
 
 
-Instruction = namedtuple('Instruction', 'operation, argument')
+@dataclass
+class Instruction:
+    operation: str
+    argument: int
 
 
 class Processor:
@@ -15,13 +18,11 @@ class Processor:
     def SwapOperation(self, instructionNumber):
         if instructionNumber in range(len(self.instructions)):
             if self.instructions[instructionNumber].operation == "jmp":
-                self.instructions[instructionNumber] = Instruction("nop",
-                    self.instructions[instructionNumber].argument)
+                self.instructions[instructionNumber].operation = "nop"
             elif self.instructions[instructionNumber].operation == "nop":
-                self.instructions[instructionNumber] = Instruction("jmp",
-                    self.instructions[instructionNumber].argument)
+                self.instructions[instructionNumber].operation = "jmp"
 
-    def Run(self):
+    def Run(self) -> int:
         self.executedLines = set()
         instructionPointer = 0
         accumulator = 0
@@ -36,9 +37,22 @@ class Processor:
                 instructionPointer += 1
             elif instruction.operation == 'jmp':
                 instructionPointer += instruction.argument
-            gotToEnd = instructionPointer == len(self.instructions)
+            self.gotToEnd = instructionPointer == len(self.instructions)
 
-        return (accumulator, gotToEnd)
+        return accumulator
+
+    def FixBadInstruction(self) -> bool:
+        lastSwapped = -1
+        while lastSwapped < len(self.instructions):
+            self.Run()
+            if self.gotToEnd:
+                break
+            self.SwapOperation(lastSwapped)
+            lastSwapped += 1
+            while lastSwapped < len(self.instructions) and self.instructions[lastSwapped].operation == 'acc':
+                lastSwapped += 1
+            self.SwapOperation(lastSwapped)
+        return lastSwapped < len(self.instructions)
 
 
 TEST = """nop +0
@@ -54,28 +68,16 @@ acc +6"""
 if __name__ == "__main__":
     testProcessor = Processor(TEST.splitlines())
     result = testProcessor.Run()
-    assert result[0] == 5, f"Part 1 test is {result}. Should be 5."
-    testProcessor.SwapOperation(7)
+    assert result == 5, f"Part 1 test is {result}. Should be 5."
+    testProcessor.FixBadInstruction()
     result2 = testProcessor.Run()
-    assert result2[0] == 8, f"Part 2 test is {result}. Should be 8."
+    assert result2 == 8, f"Part 2 test is {result}. Should be 8."
 
     with open("8.txt", "r") as infile:
         processor = Processor(infile.read().splitlines())
     part1 = processor.Run()
-    print(f"Part 1: {part1[0]}")
-    assert part1[0] == 2058, f"Part 1 is broken. Should be 2058 but is {part1[0]}."
-    
-    # Brute force the solution
-    lastSwapped = -1
-    while lastSwapped < len(processor.instructions):
-        part2 = processor.Run()
-        if part2[1]:
-            break
-        processor.SwapOperation(lastSwapped)
-        lastSwapped += 1
-        while lastSwapped < len(processor.instructions) and processor.instructions[lastSwapped].operation == 'acc':
-            lastSwapped += 1
-        processor.SwapOperation(lastSwapped)
-    
-    print(f"Part 2: {part2[0]}")
-    
+    print(f"Part 1: {part1}")
+    assert part1 == 2058, f"Part 1 is broken. Should be 2058 but is {part1}."
+    processor.FixBadInstruction()
+    part2 = processor.Run()
+    print(f"Part 2: {part2}")
