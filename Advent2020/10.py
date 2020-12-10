7070aca3927b98bd7dd27685a00fd9885cab9737
Steve Nicholson <SSteve@users.noreply.github.com>
2020-12-09 23:13:08 -0800

    2020 Day 10 Python Part 1
    In bed on the iPad screen

---
+++ Advent2020/10.py
@@ -0,0 +1,64 @@
+TEST1 = """16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4"""
+
+TEST2 = """28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"""
+
+def Part1(valueStrings):
+    values = [int(value) for value in valueStrings]
+    values.append(0)
+    values.append(max(values) + 3)
+    values = sorted(values)
+    ones = 0
+    threes = 0
+    for i in range(1, len(values)):
+        difference = values[i] - values[i - 1]
+        if difference == 1:
+            ones += 1
+        elif difference == 3:
+            threes += 1
+    return ones * threes
+
+print(Part1(TEST1.splitlines()))
+print(Part1(TEST2.splitlines()))
+with open("10.txt", "r") as infile:
+    values = infile.read().splitlines()
+print(f"Part 1: {Part1(values)}")
