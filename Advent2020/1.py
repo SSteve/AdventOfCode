b4872e65d042d8c6e9a92e1a7776f5e641dcd263
Steve Nicholson <SSteve@users.noreply.github.com>
2020-12-16 05:25:20 -0800

    Use itertools
    I was still shaking off the rust with Python on Day 1 and forgot about
    itertools.

--- Advent2020/1.py
+++ Advent2020/1.py
@@ -1,26 +1,29 @@
-def part1(values):
-    for lineNo, value1 in enumerate(values):
-        for value2 in values[lineNo + 1:]:
-            if value1 + value2 == 2020:
-                print(value1 * value2)
-                return
+from itertools import combinations
+
+
+def Part1(values):
+    for value1, value2 in combinations(values, 2):
+        if value1 + value2 == 2020:
+            return value1 * value2
                 
                 
-def part2(values):
-    for lineNo1, value1 in enumerate(values):
-        for lineNo2, value2 in enumerate(values[lineNo1 + 1:]):
-            for value3 in values[lineNo2 + 1:]:
-                if value1 + value2 + value3 == 2020:
-                    print(value1 * value2 * value3)
-                    return
+def Part2(values):
+    for value1, value2, value3 in combinations(values, 3):
+        if value1 + value2 + value3 == 2020:
+            return value1 * value2 * value3
                     
                     
 if __name__ == "__main__":
     values = []
     with open("1.txt", "r") as infile:
-        for value in infile:
-            if len(value.strip()) > 0:
-                values.append(int(value.strip()))
-    part1(values)
-    part2(values)
+        for value in infile.read().splitlines():
+            if len(value) > 0:
+                values.append(int(value))
+                
+    part1 = Part1(values)
+    print(f"Part 1: {part1}")
+    assert part1 == 703131
 
+    part2 = Part2(values)
+    print(f"Part 2: {part2}")
+    assert part2 == 272423970
