d179e044e89322c2ffc49d819730e36373e828be
Steve Nicholson <ssteve@mac.com>
2020-12-17 23:46:53 -0800

    2020 Day 15 Python I was falling asleep while I wrote this so it's probably pretty ugly.

--- Advent2020/15.py
+++ Advent2020/15.py
@@ -1,25 +1,51 @@
-def Day15(seed: str, limit: int = 2020) -> int:
-    history = [int(num) for num in seed.split(",")]
-    for _ in range(limit - len(history)):
-        lastNumber = history[-1:][0]
-        if lastNumber in history[:-1]:
-            indexPos = len(history) - history[-2::-1].index(lastNumber) - 2
-            sinceLast = len(history) - indexPos - 1
-            history.append(sinceLast)
+from typing import Dict, Tuple
+
+
+def Day15(seed: str, limit: int = 2020) -> Tuple[int, int]:
+    numbers: Dict[int, int] = {}
+    turn = 0
+    lastNumber = 0
+    part1 = -1
+    part2 = -1
+    lastWasFound = False
+    for numStr in seed.split(","):
+        turn += 1
+        lastNumber = int(numStr)
+        lastWasFound = lastNumber in numbers
+        if lastWasFound:
+            nextNumber = turn - numbers[lastNumber] - 1
         else:
-            history.append(0)
-    return history[-1:][0]
+            nextNumber = 0
+        numbers[lastNumber] = turn
+        lastNumber = nextNumber
+
+    while turn <= limit:
+        turn += 1
+        lastWasFound = lastNumber in numbers
+        if lastWasFound:
+            nextNumber = turn - numbers[lastNumber]
+        else:
+            nextNumber = 0
+        numbers[lastNumber] = turn
+
+        if turn == 2020:
+            part1 = lastNumber
+        if turn == 30_000_000:
+            part2 = lastNumber
+
+        lastNumber = nextNumber
+
+    return part1, part2
 
 
-assert Day15("0,3,6") == 436
-assert Day15("1,3,2") == 1
-assert Day15("2,1,3") == 10
-assert Day15("1,2,3") == 27
-assert Day15("2,3,1") == 78
-assert Day15("3,2,1") == 438
-assert Day15("3,1,2") == 1836
+assert Day15("0,3,6")[0] == 436
+assert Day15("1,3,2")[0] == 1
+assert Day15("2,1,3")[0] == 10
+assert Day15("1,2,3")[0] == 27
+assert Day15("2,3,1")[0] == 78
+assert Day15("3,2,1")[0] == 438
+assert Day15("3,1,2")[0] == 1836
 
-assert Day15("0,3,6") == 436
-
-print(Day15("1,17,0,10,18,11,6"))
-print(Day15("1,17,0,10,18,11,6", 30000000))
+part1, part2 = Day15("1,17,0,10,18,11,6", 30_000_000)
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
