a9864d8fbfde9c256973395fc2a69dbd146bcaa2
Steve Nicholson <ssteve@mac.com>
2020-12-23 23:17:38 -0800

    2020 Day 24 Python (experimenting)

---
+++ Advent2020/24.py
@@ -0,0 +1,36 @@
+import re
+
+regex = r"(nw|ne|sw|se|e|w)"
+
+TEST = """sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+
+
+def Traverse(line: str):
+    matches = re.finditer(regex, line)
+    for matchNum, match in enumerate(matches, start=1):
+        print(f"{match.group()} ", end='')
+
+
+for lineno, testLine in enumerate(TEST.splitlines()):
+    print(f"Line {lineno + 1}")
+    Traverse(testLine)
+    print()
