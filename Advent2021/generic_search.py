d0fb88f7102dfada2bdb39aeb51bcdf4c92cee8e
Steve Nicholson <ssteve@mac.com>
2021-12-15 09:33:40 -0800

    Add generic_search and update to newer Python typing conventions.

---
+++ Advent2021/generic_search.py
@@ -0,0 +1,167 @@
+# Adapted from Classic Computer Science Problems in Python
+# (https://www.manning.com/books/classic-computer-science-problems-in-python)
+# by David Kopec
+from collections import deque
+from typing import TypeVar, Generic, Callable, Optional
+from heapq import heappush, heappop
+
+T = TypeVar('T')
+
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: list[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container  # not is true for empty container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: deque[T] = deque()
+
+    @property
+    def empty(self) -> bool:
+        return not self._container  # not is true for empty container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.popleft()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Node(Generic[T]):
+    def __init__(self, state: T, parent: Optional['Node'], cost: float = 0.0, heuristic: float = 0.0) -> None:
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+    def __lt__(self, other: 'Node') -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+    def __repr__(self):
+        return f"Node(state={self.state}, cost={self.cost}, heuristic={self.heuristic}"
+
+
+def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], list[T]]) -> Optional[Node[T]]:
+    # frontier is where we've yet to go
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    # explored is where we've been
+    explored: set[T] = {initial}
+
+    # keep going while there is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we're done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            if child in explored:  # skip children we already explored
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None  # went through everything and never found goal
+
+
+def nodeToPath(node: Node[T]) -> list[T]:
+    path: list[T] = [node.state]
+    # work backwards from end to front
+    while node.parent is not None:
+        node = node.parent
+        path.append(node.state)
+    path.reverse()
+    return path
+
+
+def costList(node: Node) -> list[float]:
+    costs: list[float] = [node.cost]
+    while node.parent is not None:
+        node = node.parent
+        costs.append(node.cost)
+    costs.reverse()
+    return costs
+
+
+def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], list[T]]) -> Optional[Node[T]]:
+    # frontier is where we've yet to go
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node(initial, None))
+    # explored is where we've been
+    explored: set[T] = {initial}
+
+    # keep going while there is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we're done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            if child in explored:  # skip children we already explored
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None  # went through everything and never found goal
+
+
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: list[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container  # not is true for empty container
+
+    def push(self, item: T) -> None:
+        heappush(self._container, item)  # in by priority
+
+    def pop(self) -> T:
+        return heappop(self._container)  # out by priority
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], list[T]],
+          heuristic: Callable[[T], float], cost: Callable[[T, T], float]) -> Optional[Node[T]]:
+    # frontier is where we've yet to go
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+    # explored is where we've been
+    explored: dict[T, float] = {initial: 0.0}
+
+    # keep going while there is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we're done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            new_cost: float = current_node.cost + cost(child, current_state)
+
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node,
+                              new_cost, heuristic(child)))
+    return None  # went through everything and never found goal
