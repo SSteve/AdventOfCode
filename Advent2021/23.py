ee42892c1243ae82d4966f99be1ced0b62fa59a1
Steve Nicholson <ssteve@mac.com>
2021-12-25 21:35:54 -0800

    2021 Day 23 Python part 1

--- Advent2021/23.py
+++ Advent2021/23.py
@@ -1,7 +1,7 @@
-
 from __future__ import annotations
+from collections import defaultdict
 from dataclasses import dataclass
-from typing import Iterable, Iterator, Literal, Optional, Tuple
+from typing import Iterable, Optional
 
 from generic_search import astar, nodeToPath
 
@@ -21,7 +21,6 @@ class Point:
         if self.y == other.y:
             return self.x < other.x
         return self.y < other.y
-        
 
 
 def ManhattanDistance(p1: Point, p2: Point) -> int:
@@ -39,6 +38,48 @@ class AmphipodState:
     d1: Point
     d2: Point
 
+    def AllPositions(self) -> set[Point]:
+        return {self.a1, self.a2, self.b1, self.b2, self.c1, self.c2, self.d1, self.d2}
+
+    def AsDict(self) -> dict[str, list[Point]]:
+        return {'A': [self.a1, self.a2],
+                'B': [self.b1, self.b2],
+                'C': [self.c1, self.c2],
+                'D': [self.d1, self.d2],
+                }
+
+    def AtPoint(self, p: Point) -> Optional[str]:
+        dict = self.AsDict()
+        for c in dict:
+            if p in dict[c]:
+                return c
+        return None
+
+    @staticmethod
+    def FromText(lines: list[str]) -> AmphipodState:
+        positions: dict[str, list[Point]] = defaultdict(list[Point])
+        for x in range(3, 11, 2):
+            for y in range(2, 4):
+                # Our origin is the first empty hallway space, so the amphipod
+                # rooms are at x = 2, 4, 6, & 8 and y = -1 & -2
+                positions[lines[y][x]].append(Point(x - 1, -y + 1))
+        for c in positions:
+            positions[c].sort
+        return AmphipodState.FromDict(positions)
+
+    @staticmethod
+    def FromDict(positions: dict[str, list[Point]]) -> AmphipodState:
+        return AmphipodState(
+            positions['A'][0],
+            positions['A'][1],
+            positions['B'][1],
+            positions['B'][0],
+            positions['C'][0],
+            positions['C'][1],
+            positions['D'][0],
+            positions['D'][1],
+        )
+
 
 homeX: dict[str, int] = {'A': 2, 'B': 4, 'C': 6, 'D': 8}
 
@@ -48,10 +89,15 @@ hallXPositions: list[int] = [0, 1, 3, 5, 7, 9, 10]
 
 
 def IsComplete(state: AmphipodState) -> bool:
-    for c in state.keys():
-        if state[c][0] != Point(homeX[c], -2) or state[c][1] != Point(homeX[c], -1):
+    dict = state.AsDict()
+    for c in dict.keys():
+        if Point(homeX[c], -2) not in dict[c] or Point(homeX[c], -1) not in dict[c]:
             return False
     return True
+
+
+def Heuristic0(state: AmphipodState) -> float:
+    return 0
 
 
 def Heuristic(state: AmphipodState) -> float:
@@ -59,7 +105,7 @@ def Heuristic(state: AmphipodState) -> float:
     The heuristic cost is the shortest path of each amphipod to its home.
     """
     cost = 0
-    for c, positions in state.items():
+    for c, positions in state.AsDict().items():
         for i, position in enumerate(positions):
             # The home y positions are -2 and -1, so they are i - 2 since i will be 0 and 1.
             cost += ManhattanDistance(position,
@@ -67,44 +113,16 @@ def Heuristic(state: AmphipodState) -> float:
     return cost
 
 
-def AllPositions(state: AmphipodState) -> set[Point]:
-    allPositions: set[Point] = set()
-    for values in state.values():
-        allPositions.update(values)
-    return allPositions
-
-
-def AmphipodStateFromText(lines: list[str]) -> AmphipodState:
-    state: AmphipodState = {}
-    for c in ['A', 'B', 'C', 'D']:
-        state[c] = []
-    for x in range(3, 11, 2):
-        for y in range(2, 4):
-            # Our origin is the first empty hallway space, so the amphipod
-            # rooms are at x = 2, 4, 6, & 8 and y = -1 & -2
-            state[lines[y][x]].append(Point(x - 1, -y + 1))
-    for c in state:
-        state[c].sort()
-    return state
-
-
-def AtPoint(state: AmphipodState, p: Point) -> Optional[str]:
-    for c in state:
-        if p in state[c]:
-            return c
-    return None
-
-
 def Cost(start: AmphipodState, end: AmphipodState) -> float:
-    startPoints = AllPositions(start).difference(AllPositions(end))
-    endPoints = AllPositions(end).difference(AllPositions(start))
+    startPoints = start.AllPositions().difference(end.AllPositions())
+    endPoints = end.AllPositions().difference(start.AllPositions())
     if len(startPoints) != 1 or len(endPoints) != 1:
         raise ValueError(
             "There can only be one amphipod in a different location between successive states.")
     startPoint = startPoints.pop()
     endPoint = endPoints.pop()
-    startValue = AtPoint(start, startPoint)
-    endValue = AtPoint(end, endPoint)
+    startValue = start.AtPoint(startPoint)
+    endValue = end.AtPoint(endPoint)
     if startValue is None:
         raise ValueError(f"Nothing found at {startPoint}.")
     if endValue is None:
@@ -115,13 +133,11 @@ def Cost(start: AmphipodState, end: AmphipodState) -> float:
     return ManhattanDistance(startPoint, endPoint) * costs[startValue]
 
 
-def ReplacePosition(oldState: AmphipodState, c: str, x: int, y: int, i: int) -> AmphipodState:
-    newState = oldState.copy()
-    positions = newState[c][:]
-    positions[i] = Point(x, y)
-    positions.sort()
-    newState[c] = positions
-    return newState
+def ReplacePosition(state: AmphipodState, c: str, x: int, y: int, i: int) -> AmphipodState:
+    newState = state.AsDict()
+    newState[c][i] = Point(x, y)
+    newState[c].sort()
+    return AmphipodState.FromDict(newState)
 
 
 def MoveHome(state: AmphipodState, c: str, i: int, position: Point) -> Optional[AmphipodState]:
@@ -129,7 +145,7 @@ def MoveHome(state: AmphipodState, c: str, i: int, position: Point) -> Optional[
     Given an amphipod in the hall, move it home if possible. This isn't iterable
     because there's only one possible move.
     """
-    bottomPosition = AtPoint(state, Point(homeX[c], -2))
+    bottomPosition = state.AtPoint(Point(homeX[c], -2))
     # If the bottom position has a character that isn't at home, return None.
     if bottomPosition and bottomPosition != c:
         return None
@@ -142,7 +158,7 @@ def MoveHome(state: AmphipodState, c: str, i: int, position: Point) -> Optional[
     else:
         # Now we know the bottom position is a character at home. If the top position
         # is empty, it is the target.
-        topPosition = AtPoint(state, Point(homeX[c], -1))
+        topPosition = state.AtPoint(Point(homeX[c], -1))
         if topPosition:
             # There's something in the top position that is not a character at home.
             # We can't move here.
@@ -150,7 +166,7 @@ def MoveHome(state: AmphipodState, c: str, i: int, position: Point) -> Optional[
         targetY = -1
     for testX in range(position.x - 1, homeX[c], -1) if position.x > homeX[c] else range(position.x + 1, homeX[c]):
         # Look to see if there's something between here and the home position.
-        if AtPoint(state, Point(testX, 0)):
+        if state.AtPoint(Point(testX, 0)):
             return None
     return ReplacePosition(state, c, homeX[c], targetY, i)
 
@@ -164,24 +180,24 @@ def MoveToHall(state: AmphipodState, c: str, i: int, position: Point) -> Iterabl
         return None
     if position == Point(homeX[c], -1):
         # If it's in the top slot, it's only home if the bottom is the same character.
-        bottomChar = AtPoint(state, Point(homeX[c], -2))
+        bottomChar = state.AtPoint(Point(homeX[c], -2))
         if bottomChar == c:
             return None
-    if position.y == -2 and Point(position.x, -1) in AllPositions(state):
+    if position.y == -2 and Point(position.x, -1) in state.AllPositions():
         # This amphipod is in the bottom slot of a room and the upper slot is occupied.
         return None
     # At this point we know the amphipod can get out of the room. Now we return all the
     # hallway positions it can reach.
     for testX in range(position.x - 1, -1, -1):
         # Look at the positions to the left.
-        if Point(testX, 0) in AllPositions(state):
+        if Point(testX, 0) in state.AllPositions():
             # If we've encountered another amphipod in the hall, stop looking in this direction.
             break
         if testX in hallXPositions:
             yield ReplacePosition(state, c, testX, 0, i)
     for testX in range(position.x + 1, 11):
         # Look at the positions to the right.
-        if Point(testX, 0) in AllPositions(state):
+        if Point(testX, 0) in state.AllPositions():
             # If we've encountered another amphipod in the hall, stop looking in this direction.
             break
         if testX in hallXPositions:
@@ -190,8 +206,9 @@ def MoveToHall(state: AmphipodState, c: str, i: int, position: Point) -> Iterabl
 
 def Successors(state: AmphipodState) -> Iterable[AmphipodState]:
     # Find amphipods in side rooms that can move to the hall.
-    for c in state:
-        for i, position in enumerate(state[c]):
+    dict = state.AsDict()
+    for c in dict:
+        for i, position in enumerate(dict[c]):
             if position.y == 0:
                 newState = MoveHome(state, c, i, position)
                 if newState is not None:
@@ -206,14 +223,14 @@ def PrintAmphipodState(state: AmphipodState) -> None:
     print('#############')
     print('#', end='')
     for x in range(0, 11):
-        c = AtPoint(state, Point(x, 0))
+        c = state.AtPoint(Point(x, 0))
         print(c if c else '.', end='')
     print('#')
     # Third and fourth rows
     for y in range(-1, -3, -1):
         print('###' if y == -1 else '  #', end='')
         for x in range(2, 10, 2):
-            c = AtPoint(state, Point(x, y))
+            c = state.AtPoint(Point(x, y))
             print(c if c else '.', end='')
             print('#', end='')
         print('##' if y == -1 else '')
@@ -221,11 +238,28 @@ def PrintAmphipodState(state: AmphipodState) -> None:
     print('  #########\n')
 
 
-if __name__ == "__main__":
-    pods = AmphipodStateFromText(TEST.splitlines())
+def FindCost(lines: list[str], showSolution: bool = False) -> int:
+    pods = AmphipodState.FromText(lines)
     solution = astar(pods, IsComplete, Successors, Heuristic, Cost)
-    if solution:
-        PrintAmphipodState(solution.state)
-        print(f"Energy: {solution.cost}")
-    else:
-        print("No solution found.")
+    if solution is None:
+        raise ValueError("No solution found.")
+    previous = None
+    if showSolution:
+        for state in nodeToPath(solution):
+            if previous:
+                print(f"Cost: {Cost(previous, state)}")
+            previous = state
+            PrintAmphipodState(state)
+    return int(solution.cost)
+
+
+if __name__ == "__main__":
+    part1 = FindCost(TEST.splitlines(), False)
+    assert part1 == 12521
+
+    with open("23.txt", "r") as infile:
+        part1 = FindCost(infile.readlines(), False)
+    # 18328 too high.
+    # 18282 is correct. My initial Heuristic calculation was incorrect.
+    # It needed the lists of points to be sorted.
+    print(f"Part 1: {part1}")
