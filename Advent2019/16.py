5d1a4d3d9ede4bac6c32512c5fe2eec423119b54
Steve Nicholson <ssteve@mac.com>
2022-01-11 10:12:43 -0800

    2019 Day 16 Python

--- Advent2019/16.py
+++ Advent2019/16.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import Iterable
 
 BASE_PATTERN = [0, 1, 0, -1]
 
-def pattern_generator(element_number: int) -> int:
+
+def pattern_generator(element_number: int) -> Iterable[int]:
     # Return the first value element_number times
     for _ in range(element_number):
         yield BASE_PATTERN[0]
@@ -13,24 +14,46 @@ def pattern_generator(element_number: int) -> int:
             yield BASE_PATTERN[pattern_index]
         pattern_index = (pattern_index + 1) % 4
 
-def create_input_signal(input_string: str) -> List[int]:
+
+def create_input_signal(input_string: str) -> list[int]:
     return [int(c) for c in input_string]
 
-def perform_phase(input_signal: List[int]) -> List[int]:
-    result: List[int] = []
-    element_number = 0
-    for _ in range(len(input_signal)):
-        index = 0
-        accumulator = 0
-        pg = pattern_generator(element_number)
-        for fft_value in pg:
-            accumulator += fft_value * input_signal[index]
-            index += 1
-            if index >= len(input_signal):
-                break
-        result.append(abs(accumulator) % 10)
-        element_number += 1
-    return result
+
+def perform_phase(input_signal: list[int]) -> list[int]:
+    length = len(input_signal)
+    old = input_signal[:]
+    for i in range(length):
+        # Skip the numbers corresponding to 0 in the pattern.
+        j = i
+        step = i + 1
+        total = 0
+        while j < length:
+            # Sum all the digits where there's a 1 in the pattern.
+            total += sum(old[j: j + step])
+            # Skip all the digits where there's a 0 in the pattern.
+            j += 2 * step
+            # Sum all the digits where there's a -1 in the pattern and subtract from total.
+            total -= sum(old[j:j + step])
+            # Skip all the digits where there's a 0 in the pattern.
+            j += 2 * step
+        input_signal[i] = abs(total) % 10
+    return input_signal
+
+
+def part2_100_phases(input_signal: list[int]) -> str:
+    # Copied from https://github.com/mebeim/aoc
+    to_skip = int(''.join(map(str, input_signal[:7])))
+    assert to_skip > len(input_signal)//2
+
+    digits = (input_signal * 10000)[to_skip:]
+    length = len(digits)
+    for _ in range(100):
+        cumulative_sum = 0
+        for i in range(length - 1, -1, -1):
+            cumulative_sum += digits[i]
+            digits[i] = cumulative_sum % 10
+    return ''.join(map(str, digits[:8]))
+
 
 INPUT_SIGNAL1 = "12345678"
 INPUT_SIGNAL2 = "80871224585914546619083218645595"
@@ -63,22 +86,13 @@ if __name__ == '__main__':
         signal = perform_phase(signal)
     assert ''.join(f"{c}" for c in signal[0:8]) == "52432133"
 
-    offset = int(INPUT_SIGNAL1[0:7])
-    signal = create_input_signal(INPUT_SIGNAL1 * 10000)
-    for _ in range(100):
-        signal = perform_phase(signal)
-    assert ''.join(f"{c}" for c in signal[offset:offset + 8]) == "84462026"
-
     with open("16.txt") as infile:
         signal = create_input_signal(infile.readline().strip())
     for _ in range(100):
         signal = perform_phase(signal)
-    print(''.join(f"{c}" for c in signal[0:8]))
+    print("Part 1:", ''.join(f"{c}" for c in signal[0:8]))
 
     with open("16.txt") as infile:
-        input_signal = infile.readline().strip() * 10000
-    offset = int(input_signal[0:7])
-    signal = create_input_signal(input_signal)
-    for _ in range(100):
-        signal = perform_phase(signal)
-    print(''.join(f"{c}" for c in signal[offset:offset + 8]))
+        signal = create_input_signal(infile.readline().strip())
+    part2 = part2_100_phases(signal)
+    print(f"Part 2: {part2}")
