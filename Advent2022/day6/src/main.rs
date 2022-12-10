d0fa40a741b410e4fc51850da540d02fd358afdc
Steve Nicholson <ssteve@mac.com>
2022-12-09 23:59:32 -0800

    2022 Day 6 Rust

---
+++ Advent2022/day6/src/main.rs
@@ -0,0 +1,49 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn find_start(signal: &str, window_length: u32) -> Option<u32> {
+    let chars: Vec<char> = signal.chars().collect();
+    for (index, window) in chars.windows(window_length as usize).enumerate() {
+        let char_set: HashSet<_> = HashSet::from_iter(window.iter());
+        if char_set.len() == window_length as usize {
+            return Some(index as u32 + window_length);
+        }
+    }
+
+    None
+}
+
+fn find_start_of_packet(signal: &str) -> u32 {
+    find_start(signal, 4).unwrap()
+}
+
+fn find_start_of_message(signal: &str) -> u32 {
+    find_start(signal, 14).unwrap()
+}
+
+fn main() {
+    let tests = vec![
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7u32, 19u32),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5u32, 23u32),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6u32, 23u32),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10u32, 29u32),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11u32, 26u32),
+    ];
+
+    for test in tests {
+        let part1test = find_start_of_packet(test.0);
+        println!("Part 1 test: {}", part1test);
+        assert_eq!(part1test, test.1);
+        let part2test = find_start_of_message(test.0);
+        println!("Part 2 test: {}", part2test);
+        assert_eq!(part2test, test.2);
+    }
+
+    let day6input = fs::read_to_string("../day6.txt").expect("Unable to read input.");
+    let part1 = find_start_of_packet(&day6input);
+    println!("Part 1: {}", part1);
+    assert_eq!(part1, 1896);
+    let part2 = find_start_of_message(&day6input);
+    println!("Part 2: {}", part2);
+    assert_eq!(part2, 3452);
+}
