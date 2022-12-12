f56d812ba41538f364098a911e6d9179baeb7472
Steve Nicholson <ssteve@mac.com>
2022-12-12 12:11:46 -0800

    2022 Day 12 Part 2 Python (runs too slow)

--- Advent2022/day12.py
+++ Advent2022/day12.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import TypeVar
 
 from generic_search import bfs, nodeToPath
 
@@ -14,6 +15,12 @@ class Point:
     x: int
     y: int
 
+T = TypeVar('T')
+def rindex(lst: list[T], value: T):
+    lst.reverse()
+    i = lst.index(value)
+    lst.reverse()
+    return len(lst) - i - 1
 
 class HeightMap:
     def __init__(self, lines: list[str]) -> None:
@@ -37,7 +44,8 @@ class HeightMap:
                        Point(point.x, point.y-1), Point(point.x, point.y+1)]
         reachable_height = self.locations[point] + 1
         successors = list(filter(lambda p: p in surrounding and
-                                 self.locations[p] <= reachable_height, self.locations))
+                                p not in self.bad_starts and
+                                self.locations[p] <= reachable_height, self.locations))
         return successors
 
     def find_shortest_path_length(self) -> int:
@@ -45,6 +53,7 @@ class HeightMap:
         return len(path) - 1
 
     def find_shortest_path_from_point(self, point: Point) -> list[Point]:
+        self.bad_starts: set[Point] = set()
         solution_node = bfs(point, self.is_at_end, self.successors)
         if solution_node is None:
             raise ValueError("No solution found.")
@@ -52,13 +61,28 @@ class HeightMap:
 
     def find_shortest_path_from_a(self) -> int:
         paths: list[list[Point]] = []
+        self.bad_starts: set[Point] = set()
         for start in filter(lambda p: self.locations[p] == 0 and
+                            p not in self.bad_starts and
                             (p not in path for path in paths), self.locations):
-            paths.append(self.find_shortest_path_from_point(start))
-        # Now we have all of the paths that start at an 'a' location. The shortest
+            try:
+                paths.append(self.find_shortest_path_from_point(start))
+                print(f"length at {start}: {len(paths[-1]) - 1}")
+            except:
+                print(f"No solution found starting at {start}.")
+                self.bad_starts.add(start)
+                continue
+        print(f"Finished collecting paths. Found {len(paths)}.")
+        # Now we have all of the paths that start at an 'a' location.
+        shortest_path_length = len(self.locations)
         for path in paths:
-            pass
-        return 0
+            last_a_location = list(filter(lambda p: self.locations[p] == 0, path))[-1]
+            last_a_index = path.index(last_a_location)
+            if len(path) - last_a_index < shortest_path_length:
+                shortest_path_length = len(path) - last_a_index
+        # The shortest path includes the start point, but we don't count the
+        # start point in our solution.
+        return shortest_path_length - 1
 
 
 if __name__ == "__main__":
@@ -66,17 +90,17 @@ if __name__ == "__main__":
     part1test = height_map.find_shortest_path_length()
     print(f"Part 1 test: {part1test}")
     assert (part1test == 31)
-    # part2test = height_map.find_shortest_path_from_a()
-    # print(f"Part 2 test 1: {part2test}")
-    # assert (part2test == 29)
+    part2test = height_map.find_shortest_path_from_a()
+    print(f"Part 2 test: {part2test}")
+    assert (part2test == 29)
 
     with open("day12.txt") as infile:
         height_map = HeightMap(infile.read().splitlines())
 
-    part1 = height_map.find_shortest_path_length()
-    print(f"Part 1: {part1}")
-    assert (part1 == 456)
+    # part1 = height_map.find_shortest_path_length()
+    # print(f"Part 1: {part1}")
+    # assert (part1 == 456)
 
-    # part2 = height_map.find_shortest_path_from_a()
-    # print(f"Part 2: {part2}")
+    part2 = height_map.find_shortest_path_from_a()
+    print(f"Part 2: {part2}")
     # assert (part2 == 29)
