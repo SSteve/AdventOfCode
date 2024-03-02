44a20cd71b78761660fba0f65abe260817d137d8
Steve Nicholson <ssteve@mac.com>
2024-03-02 13:20:22 -0800

    2023 Day 7 Python

--- Advent2023/day7.py
+++ Advent2023/day7.py
@@ -20,6 +20,7 @@ class HandType(Enum):
 
 
 CARD_RANKS = "23456789TJQKA"
+CARD_RANKS_WITH_JOKER = "J23456789TQKA"
 
 
 def calculate_type(cards: str) -> HandType:
@@ -43,32 +44,66 @@ def calculate_type(cards: str) -> HandType:
     raise ValueError(f"Type couldn't be calculated from {cards}. ({counts})")
 
 
+def calculate_type_with_jokers(cards: str) -> HandType:
+    counts = Counter(cards)
+    jokers = counts["J"]
+    if jokers == 0:
+        return calculate_type(cards)
+    # Remove jokers from counts.
+    del counts["J"]
+    if len(counts) <= 1:
+        # There's one card plus jokers or five jokers.
+        return HandType.FIVE_OF_A_KIND
+    if max(counts.values()) + jokers == 4:
+        # There's four of something plus joker, so the highest possible is four-of-a-kind.
+        return HandType.FOUR_OF_A_KIND
+    if 2 in counts.values():
+        if len(counts) == 2:
+            # There's two of something, two of something else, and one joker.
+            # (If there were two of something and two jokers we would have returned four-of-a-kind.)
+            return HandType.FULL_HOUSE
+        # There's two of something and one of everything else.
+        return HandType.THREE_OF_A_KIND
+    if jokers == 2:
+        # There's one of everything. If there are two jokers, we have three-of-a-kind.
+        return HandType.THREE_OF_A_KIND
+    if len(counts) == 4:
+        # There's one of four different things and one joker.
+        return HandType.ONE_PAIR
+    # HIGH_CARD is not possible with a joker.
+
+    raise ValueError(f"Type couldn't be calculated from {cards}. ({counts})")
+
+
 class Hand:
     cards: str
     bid: int
     type: HandType
+    with_jokers: bool
 
-    def __init__(self, cards, bid) -> None:
+    def __init__(self, cards, bid, with_jokers: bool = False) -> None:
         self.cards = cards
         self.bid = int(bid)
-        self.type = calculate_type(cards)
+        self.type = calculate_type_with_jokers(cards) if with_jokers else calculate_type(cards)
+        self.with_jokers = with_jokers
 
     def __repr__(self) -> str:
         return f"{self.cards} {self.type} bid: {self.bid}"
 
     def __lt__(self, other: Self) -> bool:
         if self.type.value == other.type.value:
+            ranks = CARD_RANKS_WITH_JOKER if self.with_jokers else CARD_RANKS
             for this_rank, other_rank in zip(self.cards, other.cards):
-                if CARD_RANKS.index(this_rank) < CARD_RANKS.index(other_rank):
+                if ranks.index(this_rank) < ranks.index(other_rank):
                     return True
-                if CARD_RANKS.index(this_rank) > CARD_RANKS.index(other_rank):
+                if ranks.index(this_rank) > ranks.index(other_rank):
                     return False
             return False
         return self.type.value < other.type.value
 
 
-def calculate_winnings(hand_strings: list[str]) -> int:
-    hands = sorted(map(lambda vals: Hand(*(vals.split())), hand_strings))
+def calculate_winnings(hand_strings: list[str], with_jokers=False) -> int:
+    hands = sorted(map(lambda vals: Hand(*(vals.split()), with_jokers), hand_strings))
 
     winnings = 0
     for i, hand in enumerate(hands):
@@ -81,19 +116,16 @@ if __name__ == "__main__":
     print(f"Part 1 test: {part1test}")
     assert part1test == 6440
 
-    """     
-    part2test = record_beating_count(TEST.splitlines())
+    part2test = calculate_winnings(TEST.splitlines(), True)
     print(f"Part 2 test: {part2test}")
-    assert part2test == 71503
-    """
+    assert part2test == 5905
 
     with open("day7.txt") as infile:
         lines = infile.read().splitlines()
 
     part1 = calculate_winnings(lines)
     print(f"Part 1: {part1}")
+    assert part1 == 249390788
 
-    """ 
-    part2 = record_beating_count(lines)
+    part2 = calculate_winnings(lines, True)
     print(f"Part 2: {part2}")
-     """
