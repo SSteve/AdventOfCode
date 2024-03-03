b83d5049499d3945459f4a8ef89f71d505f36634
Steve Nicholson <ssteve@mac.com>
2024-03-02 17:06:10 -0800

    2023 Day 9 Python

--- Advent2023/day9.py
+++ Advent2023/day9.py
@@ -21,16 +21,35 @@ def sum_of_extrapolations(lines: list[str]) -> int:
     return extrapolation_sum
 
 
+def left_extrapolation(line: str) -> int:
+    values = list(map(int, line.split()))
+    final_values: list[int] = []
+
+    while any(v != 0 for v in values):
+        final_values.append(values[0])
+        values = [values[i] - values[i - 1] for i in range(1, len(values))]
+
+    difference = 0
+    while final_values:
+        difference = final_values.pop() - difference
+
+    return difference
+
+
+def sum_of_left_extrapolations(lines: list[str]) -> int:
+    extrapolation_sum = sum(left_extrapolation(line) for line in lines)
+
+    return extrapolation_sum
+
+
 if __name__ == "__main__":
     part1test = sum_of_extrapolations(TEST.splitlines())
     print(f"Part 1 test: {part1test}")
     assert part1test == 114
 
-    """ 
-    part2test = count_ghost_steps(TEST2.splitlines())
+    part2test = sum_of_left_extrapolations(TEST.splitlines())
     print(f"Part 2 test: {part2test}")
-    assert part2test == 6
-    """
+    assert part2test == 2
 
     with open("day9.txt") as infile:
         lines = infile.read().splitlines()
@@ -39,7 +58,5 @@ if __name__ == "__main__":
     print(f"Part 1: {part1}")
     assert part1 == 1789635132
 
-    """ 
-    part2 = count_ghost_steps(lines)
+    part2 = sum_of_left_extrapolations(lines)
     print(f"Part 2: {part2}")
-    """
