edbe04ceaabccf2b6df7d5d0bcea856f2b9d2704
Steve Nicholson <ssteve@mac.com>
2024-03-03 18:27:07 -0800

    2023 Day 11 Python

--- Advent2023/day11.py
+++ Advent2023/day11.py
@@ -19,7 +19,7 @@ class Point:
     y: int
 
 
-def create_universe(lines: list[str]) -> set[Point]:
+def create_universe(lines: list[str], empty_increment=1) -> set[Point]:
     universe: set[Point] = set()
 
     # Find which columns cause expansion.
@@ -33,12 +33,12 @@ def create_universe(lines: list[str]) -> set[Point]:
 
     for y, line in enumerate(lines):
         if "#" not in line:
-            y_offset += 1
+            y_offset += empty_increment
             continue
         x_offset = 0
         for x, c in enumerate(line):
             if x in empty_columns:
-                x_offset += 1
+                x_offset += empty_increment
             if c == "#":
                 universe.add(Point(x + x_offset, y + y_offset))
 
@@ -49,8 +49,8 @@ def manhattan_distance(p1: Point, p2: Point) -> int:
     return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 
-def shortest_path_length(lines: list[str]) -> int:
-    universe = create_universe(lines)
+def shortest_path_length(lines: list[str], empty_increment=1) -> int:
+    universe = create_universe(lines, empty_increment)
     total_distance = 0
     for start, end in combinations(universe, 2):
         total_distance += manhattan_distance(start, end)
@@ -62,20 +62,20 @@ if __name__ == "__main__":
     print(f"Part 1 test: {part1test}")
     assert part1test == 374
 
-    """ 
-    part2test = count_ghost_steps(TEST2.splitlines())
+    part2test = shortest_path_length(TEST.splitlines(), 9)
     print(f"Part 2 test: {part2test}")
-    assert part2test == 6
- """
+    assert part2test == 1030
+
+    part2test = shortest_path_length(TEST.splitlines(), 99)
+    print(f"Part 2 test: {part2test}")
+    assert part2test == 8410
 
     with open("day11.txt") as infile:
         lines = infile.read().splitlines()
 
     part1 = shortest_path_length(lines)
     print(f"Part 1: {part1}")
-    # assert part1 == 22411
+    assert part1 == 9403026
 
-    """ 
-    part2 = count_ghost_steps(lines)
+    part2 = shortest_path_length(lines, 1_000_000 - 1)
     print(f"Part 2: {part2}")
- """
