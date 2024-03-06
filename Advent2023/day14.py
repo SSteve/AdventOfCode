da2482a5e990ad7a29ee87904d3f4a9e4fcabbf7
Steve Nicholson <SSteve@users.noreply.github.com>
2024-03-06 08:25:21 -0800

    2023 Day 14 Python (iPad)
    Edited on iPad. Needs to be cleaned up.

--- Advent2023/day14.py
+++ Advent2023/day14.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import hashlib
 
 TEST = """O....#....
 O.OO#....#
@@ -17,7 +18,12 @@ class Point:
     x: int
     y: int
 
-
+    def __lt__(self, other: 'Point'):
+        if self.x == other.x:
+            return self.y < other.y
+        return self.x < other.x
+        
+    
 class RockMap:
     rocks: set[Point]
     cubes: set[Point]
@@ -35,6 +41,25 @@ class RockMap:
                     self.cubes.add(Point(x, y))
                 elif c == "O":
                     self.rocks.add(Point(x, y))
+                    
+    def __repr__(self):
+        repr = ''
+        for y in range(self.height):
+            for x in range(self.width):
+                point = Point(x,y)
+                if point in self.rocks:
+                    repr += 'O'
+                elif point in self.cubes:
+                    repr +='#'
+                else:
+                    repr +='.'
+            repr += '\n'
+        return repr
+                    
+    def fingerprint(self):
+        rock_list = list(sorted(self.rocks))
+        rock_string = ''.join(str(rock) for rock in rock_list)
+        return hashlib.sha256(bytes(rock_string, 'UTF-8')).hexdigest()
 
     def cube_at_coordinate(self, x: int, y: int) -> bool:
         point = Point(x, y)
@@ -49,53 +74,142 @@ class RockMap:
 
     def add_rock_at_coordinate(self, x: int, y: int) -> None:
         self.rocks.add(Point(x, y))
+        
+    def north_load(self) -> int:
+        load = 0
+        for rock in self.rocks:
+            load += self.height - rock.y
+    
+        return load
 
+        
+    def roll_north(self):
+        for x in range(self.width):
+            previous_cube = -1
+            y = 0
+            rock_coordinates: set[int] = set()
+            while y <= self.height:
+                if self.rock_at_coordinate(x, y):
+                    rock_coordinates.add(y)
+                elif self.cube_at_coordinate(x, y) or y == self.height:
+                    for rock_y in rock_coordinates:
+                        self.remove_rock_at_coordinate(x, rock_y)
+                    for rock_y in range(previous_cube + 1, previous_cube + 1 + len(rock_coordinates)):
+                        self.add_rock_at_coordinate(x, rock_y)
+                    rock_coordinates.clear()
+                    previous_cube = y
+                y += 1
 
-def calculate_north_load(lines: list[str]) -> int:
+    def roll_south(self):
+        for x in range(self.width):
+            previous_cube = self.height
+            y = self.height - 1
+            rock_coordinates: set[int] = set()
+            while y >=-1:
+                if self.rock_at_coordinate(x, y):
+                    rock_coordinates.add(y)
+                elif self.cube_at_coordinate(x, y) or y == -1:
+                    for rock_y in rock_coordinates:
+                        self.remove_rock_at_coordinate(x, rock_y)
+                    for rock_y in range(previous_cube - len(rock_coordinates), previous_cube):
+                        self.add_rock_at_coordinate(x, rock_y)
+                    rock_coordinates.clear()
+                    previous_cube = y
+                y -= 1
+                
+    def roll_west(self):
+        for y in range(self.height):
+            previous_cube = -1
+            x = 0
+            rock_coordinates: set[int] = set()
+            while x <= self.width:
+                if self.rock_at_coordinate(x, y):
+                    rock_coordinates.add(x)
+                elif self.cube_at_coordinate(x, y) or x == self.width:
+                    for rock_x in rock_coordinates:
+                        self.remove_rock_at_coordinate(rock_x, y)
+                    for rock_x in range(previous_cube + 1, previous_cube + 1 + len(rock_coordinates)):
+                        self.add_rock_at_coordinate(rock_x, y)
+                    rock_coordinates.clear()
+                    previous_cube = x
+                x += 1
+
+                
+    def roll_east(self):
+        for y in range(self.height):
+            previous_cube = self.width
+            x = self.width - 1
+            rock_coordinates: set[int] = set()
+            while x >= -1:
+                if self.rock_at_coordinate(x, y):
+                    rock_coordinates.add(x)
+                elif self.cube_at_coordinate(x, y) or x == -1:
+                    for rock_x in rock_coordinates:
+                        self.remove_rock_at_coordinate(rock_x, y)
+                    for rock_x in range(previous_cube - len(rock_coordinates), previous_cube):
+                        self.add_rock_at_coordinate(rock_x, y)
+                    rock_coordinates.clear()
+                    previous_cube = x
+                x -= 1
+
+    def cycle(self):
+        self.roll_north()
+        self.roll_west()
+        self.roll_south()
+        self.roll_east()
+                
+
+def roll_north(lines: list[str]) -> int:
     rock_map = RockMap(lines)
+    rock_map.roll_north()
+    return rock_map.north_load()
+    
+def run_cycles(lines: list[str], count: int) -> int:
+    rock_map = RockMap(lines)
+    rock_map.cycle()
+    fingerprint = rock_map.fingerprint()
+    fingerprints = {}
+    
+    
+    while fingerprint not in fingerprints:
+        fingerprints[fingerprint]=rock_map.north_load()
+        rock_map.cycle()
+        fingerprint = rock_map.fingerprint()
+        
+    """
+    for y, f in enumerate(fingerprints.items()):
+        print(y,f)
+    print(fingerprint)
+    """
+    print(len(fingerprints))
+    previous_index = list(fingerprints.keys()).index(fingerprint)
+    print(previous_index)
+    cycle_length = len(fingerprints)- previous_index
+    print(f"{cycle_length=}")
+    index = ((count - (previous_index+1))%cycle_length)+previous_index
+    print(f"{index=}")
+    key = list(fingerprints.keys())[index]
+    print(key)
+    
+    return fingerprints[key]
 
-    for x in range(rock_map.width):
-        previous_cube = -1
-        y = 0
-        rock_coordinates: set[int] = set()
-        while y <= rock_map.height:
-            if rock_map.rock_at_coordinate(x, y):
-                rock_coordinates.add(y)
-            elif rock_map.cube_at_coordinate(x, y) or y == rock_map.height:
-                for rock_y in rock_coordinates:
-                    rock_map.remove_rock_at_coordinate(x, rock_y)
-                for rock_y in range(previous_cube + 1, previous_cube + 1 + len(rock_coordinates)):
-                    rock_map.add_rock_at_coordinate(x, rock_y)
-                rock_coordinates.clear()
-                previous_cube = y
-            y += 1
-
-    load = 0
-    for rock in rock_map.rocks:
-        load += rock_map.height - rock.y
-
-    return load
-
-
-if __name__ == "__main__":
-    part1test = calculate_north_load(TEST.splitlines())
+if __name__ == "__main__":    
+    part1test = roll_north(TEST.splitlines())
     print(f"Part 1 test: {part1test}")
     assert part1test == 136
 
-    """ 
-    part2test = calculate_north_load(TEST.splitlines())
+    part2test = run_cycles(TEST.splitlines(), 1_000_000_000)
     print(f"Part 2 test: {part2test}")
-    assert part2test == 1030
- """
+    assert part2test == 64
 
     with open("day14.txt") as infile:
         lines = infile.read().splitlines()
 
-    part1 = calculate_north_load(lines)
+    part1 = roll_north(lines)
     print(f"Part 1: {part1}")
-    # assert part1 == 9403026
+    assert part1 == 103614
 
-    """ 
-    part2 = calculate_north_load(lines)
+    
+    part2 = run_cycles(lines, 1_000_000_000)
     print(f"Part 2: {part2}")
- """
+ 
