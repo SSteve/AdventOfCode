80fe783d2f06ed2fb4f398a760d81282d9b3ba84
Steve Nicholson <ssteve@mac.com>
2024-03-19 19:37:55 -0700

    2023 Day 23 Python part 2 not working

--- Advent2023/day23.py
+++ Advent2023/day23.py
@@ -66,10 +66,14 @@ class Point:
 class TrailMap:
     map: list[str]
     finish: Point
+    use_slopes: bool
+    states: set[str]
 
-    def __init__(self, input: list[str]):
+    def __init__(self, input: list[str], use_slopes):
         self.map = input
         self.finish = Point(len(input[0]) - 2, len(input) - 1)
+        self.use_slopes = use_slopes
+        self.states = set()
 
     @cache
     def location_is_on_trail(self, location: Point):
@@ -84,27 +88,26 @@ class TrailMap:
         # Return the possible next locations.
         next_locations: list[Point] = []
         current_tile = self.map[location.y][location.x]
-        match current_tile:
-            case ".":
-                for p in location.neighbors():
-                    if self.location_is_on_trail(p):
-                        next_locations.append(p)
-            case ">":
-                p = location.right()
+        if current_tile == "." or self.use_slopes is False:
+            for p in location.neighbors():
                 if self.location_is_on_trail(p):
                     next_locations.append(p)
-            case "v":
-                p = location.down()
-                if self.location_is_on_trail(p):
-                    next_locations.append(p)
-            case "<":
-                p = location.left()
-                if self.location_is_on_trail(p):
-                    next_locations.append(p)
-            case "^":
-                p = location.up()
-                if self.location_is_on_trail(p):
-                    next_locations.append(p)
+        elif current_tile == ">":
+            p = location.right()
+            if self.location_is_on_trail(p):
+                next_locations.append(p)
+        elif current_tile == "v":
+            p = location.down()
+            if self.location_is_on_trail(p):
+                next_locations.append(p)
+        elif current_tile == "<":
+            p = location.left()
+            if self.location_is_on_trail(p):
+                next_locations.append(p)
+        elif current_tile == "^":
+            p = location.up()
+            if self.location_is_on_trail(p):
+                next_locations.append(p)
 
         return next_locations
 
@@ -121,6 +124,14 @@ class TrailMap:
 
             if len(next) > 1:
                 # If this is a fork, return the longest path from here to the end.
+                state = f"{len(previous)} steps. Location: {location}. Fork: {next}"
+                # print(state)
+                if state in self.states:
+                    # raise ValueError("We've seen this state before.")
+                    # print("We've seen this state before.")
+                    return 0
+                    pass
+                self.states.add(state)
                 return max(
                     self.find_longest_hike(p, set([*previous, location])) for p in next
                 )
@@ -134,30 +145,27 @@ class TrailMap:
                     return len(previous)
 
 
-def find_longest_hike(lines: list[str]) -> int:
-    trail_map = TrailMap(lines)
+def find_longest_hike(lines: list[str], part2: bool) -> int:
+    trail_map = TrailMap(lines, part2)
     return trail_map.find_longest_hike(Point(1, 0), set())
 
 
 if __name__ == "__main__":
-    part1test = find_longest_hike(TEST.splitlines())
+    part1test = find_longest_hike(TEST.splitlines(), True)
     print(f"Part 1 test: {part1test}")
     assert part1test == 94
 
-    """ 
-    part2test = count_chain_reactions(TEST.splitlines())
+    part2test = find_longest_hike(TEST.splitlines(), False)
     print(f"Part 2 test: {part2test}")
-    assert part2test == 7
- """
+    assert part2test == 154
+
     with open("day23.txt") as infile:
         lines = infile.read().splitlines()
 
-    part1 = find_longest_hike(lines)
+    part1 = find_longest_hike(lines, True)
     print(f"Part 1: {part1}")
     assert part1 == 2402
 
-    """ 
-    part2 = count_chain_reactions(lines)
+    part2 = find_longest_hike(lines, False)
     print(f"Part 2: {part2}")
-    assert part2 == 77070
- """
+    assert part2 > 6098
