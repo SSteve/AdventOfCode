ddfafcb5d63009f7967b0a6f3d5bea57d52e6391
Steve Nicholson <ssteve@mac.com>
2024-03-23 13:45:35 -0700

    2023 Day 24 Python

--- Advent2023/day24.py
+++ Advent2023/day24.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from decimal import Decimal
 from itertools import combinations
 from typing import Self
 
@@ -40,7 +41,6 @@ class Hailstone:
 
 def count_intersections(lines: list[str], low: int, high: int) -> int:
     hailstones: set[Hailstone] = set()
-    discarded = 0
     for line in lines:
         hailstone = Hailstone.from_line(line)
         if hailstone in hailstones:
@@ -51,7 +51,6 @@ def count_intersections(lines: list[str], low: int, high: int) -> int:
             or (hailstone.y > high and hailstone.delta_y > 0)
             or (hailstone.y < low and hailstone.delta_y < 0)
         ):
-            discarded += 1
             continue
         hailstones.add(hailstone)
 
@@ -70,25 +69,87 @@ def count_intersections(lines: list[str], low: int, high: int) -> int:
     return intersections
 
 
+def elim(m: list[list[Decimal]]) -> None:
+    # Perform Gaussian elimination.
+    for i in range(len(m)):
+        t = m[i][i]
+        m[i] = [x / t for x in m[i]]
+        for j in range(i + 1, len(m)):
+            t = m[j][i]
+            m[j] = [x - t * m[i][k] for k, x in enumerate(m[j])]
+    for i in range(len(m) - 1, -1, -1):
+        for j in range(i):
+            t = m[j][i]
+            m[j] = [x - t * m[i][k] for k, x in enumerate(m[j])]
+    return m
+
+
+def part2(lines: list[str]) -> int:
+    """Based on https://www.reddit.com/r/adventofcode/comments/18q40he/2023_day_24_part_2_a_straightforward_nonsolver/
+
+    I was on the right track on this but I got bogged down in the equations so I got some help.
+    """
+    all_hailstones: set[Hailstone] = set()
+    for line in lines:
+        all_hailstones.add(Hailstone.from_line(line))
+
+    hailstones: list[Hailstone] = [all_hailstones.pop()]
+    while len(hailstones) < 5:
+        hailstone = all_hailstones.pop()
+        if len(lines) > 5 and any(h.m == hailstone.m for h in hailstones):
+            continue
+        hailstones.append(hailstone)
+
+    matrix: list[list[Decimal]] = []
+
+    h = hailstones[-1]
+    for h2 in hailstones[:-1]:
+        coords: list[Decimal] = []
+        coords.append(Decimal(h2.delta_y - h.delta_y))
+        coords.append(Decimal(h.delta_x - h2.delta_x))
+        coords.append(Decimal(h.y - h2.y))
+        coords.append(Decimal(h2.x - h.x))
+        coords.append(Decimal(h2.x * h2.delta_y - h2.y * h2.delta_x - h.x * h.delta_y + h.y * h.delta_x))
+        matrix.append(coords)
+
+    elim(matrix)
+
+    x = Decimal(round(matrix[0][-1]))
+    y = Decimal(round(matrix[1][-1]))
+
+    matrix = []
+    h = hailstones[-1]
+    for h2 in hailstones[:-1]:
+        coords: list[Decimal] = []
+        coords.append(Decimal(h2.delta_z - h.delta_z))
+        coords.append(Decimal(h.delta_x - h2.delta_x))
+        coords.append(Decimal(h.z - h2.z))
+        coords.append(Decimal(h2.x - h.x))
+        coords.append(Decimal(h2.x * h2.delta_z - h2.z * h2.delta_x - h.x * h.delta_z + h.z * h.delta_x))
+        matrix.append(coords)
+
+    elim(matrix)
+    z = Decimal(round(matrix[1][-1]))
+
+    return x + y + z
+
+
 if __name__ == "__main__":
     part1test = count_intersections(TEST.splitlines(), 7, 27)
     print(f"Part 1 test: {part1test}")
     assert part1test == 2
 
-    """ 
-    part2test = find_longest_hike2(TEST.splitlines())
+    part2test = part2(TEST.splitlines())
     print(f"Part 2 test: {part2test}")
-    assert part2test == 154
- """
+    # assert part2test == 47
+
     with open("day24.txt") as infile:
         lines = infile.read().splitlines()
 
     part1 = count_intersections(lines, 200000000000000, 400000000000000)
     print(f"Part 1: {part1}")
-    # assert part1 == 2402
+    assert part1 == 20847
 
-    """ 
-    part2 = find_longest_hike2(lines)
+    part2 = part2(lines)
     print(f"Part 2: {part2}")
-    assert part2 == 6450
- """
+    assert part2 == 908621716620524
